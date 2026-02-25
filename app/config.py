cc206bd966af298c67be7d14f790359924721ff5
chathurachandra <chathurachandr@gmail.com>
2026-02-25 22:07:35 +0530

    config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-DATABASE_URL = "mysql+pymysql://root:chathura%4024C@localhost:3306/ecommerce_db"
+DATABASE_URL = "mysql+pymysql://root:--------@localhost:3306/ecommerce_db"
 SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
